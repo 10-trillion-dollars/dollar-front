7d3d6fb349e8baf1554e2ed0d1a9d8b6605796cf
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-01 15:48:40 +0900

    :sparkles: 타임리프 + restTemplate 적용

--- src/main/java/org/example/dollar_front/controller/ShopController.java
+++ src/main/java/org/example/dollar_front/controller/ShopController.java
@@ -6,8 +6,14 @@ import org.example.dollar_front.model.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ShopController {
@@ -33,5 +39,30 @@ public class ShopController {
         }
         return "index";
     }
+
+//    @PostMapping("/login")
+//    public String handleLogin(@RequestParam String username, @RequestParam String password, RedirectAttributes redirectAttributes) {
+//        // RestTemplate을 사용하여 인증 서버에 로그인 요청
+//        RestTemplate restTemplate = new RestTemplate();
+//        String url = "http://localhost:8082/users/login";
+//        // 로그인 요청에 필요한 데이터 설정
+//        MultiValueMap<String, String> requestMap = new LinkedMultiValueMap<>();
+//        requestMap.add("email", username);
+//        requestMap.add("password", password);
+//
+//        try {
+//            ResponseEntity<String> response = restTemplate.postForEntity(url, requestMap, String.class);
+//            // 인증 성공 시 처리 로직
+//            // 예: 세션에 사용자 정보 저장, 성공 메시지 설정 등
+//            return "redirect:/"; // 홈 페이지로 리다이렉트
+//        } catch (HttpClientErrorException e) {
+//            // 인증 실패 시 처리 로직
+//            // 예: 실패 메시지 설정, 로그인 페이지로 리다이렉트
+//            System.out.println("im work");
+//            redirectAttributes.addFlashAttribute("error", "로그인 실패");
+//            return "redirect:/signupin"; // 로그인 페이지로 리다이렉트
+//        }
+//    }
+
 
 }
